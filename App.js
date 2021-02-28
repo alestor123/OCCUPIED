4b9846399c1f2b6eed9213eb8852c54e6eb43393
alestor123 <alestoraldous@gmail.com>
2021-02-28 13:45:16 +0530

    added main code

---
+++ App.js
@@ -0,0 +1,10 @@
+var axios = require('axios');
+module.exports = async name => {
+if(!name) throw Error('Please enter the name')
+try {
+if(await axios.get(`https://registry.npmjs.org/${name}`)) return true
+}
+catch(err) {
+return !err.response.status==404
+}
+}
